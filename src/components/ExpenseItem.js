8bf7fea95943a9871c93d393887e863f01505270
JaeHyuk <kwon850550@naver.com>
2022-09-24 00:43:16 +0900

    ctrl + k +f로 자동정렬

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,14 @@
-function ExpenseItem(){
-    return (<div>  
-        <div>Date</div> 
-        <div><h2>Title</h2><div> Amount</div>
-        </div>
-    </div>)
-} 
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div> Amount</div>
+      </div>
+    </div>
+  );
+}
 // ()로 묶는 두번째 방법
 
 export default ExpenseItem;
