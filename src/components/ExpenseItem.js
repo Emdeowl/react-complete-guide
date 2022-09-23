9aae1859af0948d87eb3c9fda3d1ab550d86efb1
JaeHyuk <kwon850550@naver.com>
2022-09-24 00:39:14 +0900

    두번째 방법

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,10 @@
 function ExpenseItem(){
-    return <div>  
+    return (<div>  
         <div>Date</div> 
         <div><h2>Title</h2><div> Amount</div>
         </div>
-    </div>
+    </div>)
 } 
-// 첫번째 방법
+// ()로 묶는 두번째 방법
 
 export default ExpenseItem;
