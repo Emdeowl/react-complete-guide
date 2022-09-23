668918cd5dd8177b8f86b554ad895b568ac6b7ff
JaeHyuk <kwon850550@naver.com>
2022-09-24 00:38:39 +0900

    첫번쨰 방법

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
   <div>
     <h2>Let's get started</h2>
-    <p> This is also visiable</p>
+   <ExpenseItem></ExpenseItem>
   </div>
   );
-}
 
+}
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return <div>  
+        <div>Date</div> 
+        <div><h2>Title</h2><div> Amount</div>
+        </div>
+    </div>
+} 
+// 첫번째 방법
+
+export default ExpenseItem;
