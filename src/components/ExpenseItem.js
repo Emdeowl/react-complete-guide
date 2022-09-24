5ff9048631e10af659fd541194f2ae8ee46f01f8
JaeHyuk <kwon850550@naver.com>
2022-09-24 17:07:21 +0900

    className 충돌 회피

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 function Expenses(props) {
 
 
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'> ${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 // ()로 묶는 두번째 방법
