806f5a75776873ba44b4f04d695594ed041deff0
JaeHyuk <kwon850550@naver.com>
2022-09-25 19:43:55 +0900

    1. select에 속성 = {} 한다. 2. expenses.js에 가서 속성={} 부모느낌 3. props통해 filter.js에 값을 준다.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,35 +1,42 @@
 import React from 'react';
-import './Expense.css'
-import ExpenseItem from './ExpenseItem'
-import Card from '../UI/Card';
 
-const Expenses  = (props) =>
-{    
-    return(
-        <Card className='expenses'>
-            <ExpenseItem
-                title={props.item[0].title}
-                amount={props.item[0].amount}
-                date={props.item[0].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                title={props.item[1].title}
-                amount={props.item[1].amount}
-                date={props.item[1].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                title={props.item[2].title}
-                amount={props.item[2].amount}
-                date={props.item[2].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                title={props.item[3].title}
-                amount={props.item[3].amount}
-                date={props.item[3].date}
-            ></ExpenseItem>
-            </Card>
-        
-    )
-}
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expense.css';
+
+const Expenses = (props) => {
+    const filterChangeHandler=selectedYear=>{
+        console.log('expenses.js')
+        console.log(selectedYear);
+    }
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter onChangeFliter={filterChangeHandler} />
+        <ExpenseItem
+          title={props.item[0].title}
+          amount={props.item[0].amount}
+          date={props.item[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.item[1].title}
+          amount={props.item[1].amount}
+          date={props.item[1].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.item[2].title}
+          amount={props.item[2].amount}
+          date={props.item[2].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.item[3].title}
+          amount={props.item[3].amount}
+          date={props.item[3].date}
+        ></ExpenseItem>
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
