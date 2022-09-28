731b7b062415eb2d9a56de83adbed5419bfed3b0
JaeHyuk <kwon850550@naver.com>
2022-09-28 19:03:47 +0900

    expenses.js를 간편화 하기 위해 list를 만들고 조건문을 옮김

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
+import ExpensesList from ',/ExpensesList'
 import ExpensesFilter from './ExpensesFilter';
+import Card from '../UI/Card';
 import './Expenses.css';
+
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState('2020');
@@ -18,19 +19,8 @@ const Expenses = (props) => {
   // filter라는 내장된 메소드를 실행.  true가 되면 반환한다.  그러기위해 return에 조건을 넣어줌
   //expense에 있는 date의 year를 4자리로 받아서 string으로 만든것과 선택된 연도가 같다면~~
 
-  let expenseContent = <p>No expense found</p>;
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
-  //JSX에 있던 코드를 변수로 지정하고 if 조건문을 통하여 동적으로 변하게  만듬. 그래서 let 변수를 쓴거
+ 
+ 
 
   return (
     <div>
@@ -39,7 +29,7 @@ const Expenses = (props) => {
           selected={filterYear}
           onChangeFliter={filterChangeHandler}
         />
-        {expenseContent}
+       <ExpensesList item={} />
       </Card>
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,22 +1,33 @@
+import React, { useState } from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
 
-const NewExpense = (props)=> {
-    const SaveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData={
-            ...enteredExpenseData,
-            id:  Math.random().toString()
-        } 
-      
-       props.onAddExpense(expenseData);
-    }
-    return(
-        <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData ={SaveExpenseDataHandler}/>
-        </div>
-    )
+  const SaveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
 
-}
+  const startEditingHandler = () => {
+    setIsEditing(true);  // 이 함수가 실행이 되면 화면에 보여야한다.
+  };
+
+  const stopEditingHandler = ()=>{
+    setIsEditing(false);
+  }
+  
+  return (
+    <div className="new-expense">
+      {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>} 
+      {isEditing && <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler}  onCancel={stopEditingHandler}/>}
+    </div>
+  );
+};
 
 export default NewExpense;
