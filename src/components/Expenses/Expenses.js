279263af077323d6f7d36950a9e85cd50805c3b6
JaeHyuk <kwon850550@naver.com>
2022-09-28 18:03:33 +0900

    조건문

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,14 +10,13 @@ const Expenses = (props) => {
 
   const filterChangeHandler = (selectedYear) => {
     setFilterYear(selectedYear);
-  }
+  };
 
-  const filteredExpenses = props.item.filter(expense => {
+  const filteredExpenses = props.item.filter((expense) => {
     return expense.date.getFullYear().toString() === filterYear;
-  })
-// filter라는 내장된 메소드를 실행.  true가 되면 반환한다.  그러기위해 return에 조건을 넣어줌
-//expense에 있는 date의 year를 4자리로 받아서 string으로 만든것과 선택된 연도가 같다면~~
-
+  });
+  // filter라는 내장된 메소드를 실행.  true가 되면 반환한다.  그러기위해 return에 조건을 넣어줌
+  //expense에 있는 date의 year를 4자리로 받아서 string으로 만든것과 선택된 연도가 같다면~~
 
   return (
     <div>
@@ -26,18 +25,21 @@ const Expenses = (props) => {
           selected={filterYear}
           onChangeFliter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredExpenses.length === 0 ? (
+          <p>NO expense found</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
 };
-
 
 export default Expenses;
