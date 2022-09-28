03f07bd3fd6318bd14d8bc8bed448274e31c1d1d
JaeHyuk <kwon850550@naver.com>
2022-09-28 18:00:09 +0900

    filterExpense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,6 +15,9 @@ const Expenses = (props) => {
   const filteredExpenses = props.item.filter(expense => {
     return expense.date.getFullYear().toString() === filterYear;
   })
+// filter라는 내장된 메소드를 실행.  true가 되면 반환한다.  그러기위해 return에 조건을 넣어줌
+//expense에 있는 date의 year를 4자리로 받아서 string으로 만든것과 선택된 연도가 같다면~~
+
 
   return (
     <div>
