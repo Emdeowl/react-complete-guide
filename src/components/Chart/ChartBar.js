f1aaa09c6da3341786fbd72f2ef8b73eaf1dc602
JaeHyuk <kwon850550@naver.com>
2022-09-29 17:14:25 +0900

    css연결 실수 해결 데이터가 안나와

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -4,6 +4,7 @@ import './ChartBar.css';
 
 const ChartBar = (props) => {
   let barFillHeight = '0%';
+  
   if (props.max > 0) {
     // Chart에 데이터가 있다면
     barFillHeight = Math.round((props.value / props.maxValue) * 100 + '%');
@@ -11,7 +12,7 @@ const ChartBar = (props) => {
 
   return (
     <div className="chart-bar">
-      <div className="chart-bar__-inner">
+      <div className="chart-bar__inner">
         <div
           className="chart-bar__fill"
           style={{ height: barFillHeight }} //css를 동적으로 설정
