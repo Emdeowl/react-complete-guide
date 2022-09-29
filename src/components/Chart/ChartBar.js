a84aa16e5ed43880e9b549da41200c8d46d320eb
JaeHyuk <kwon850550@naver.com>
2022-09-29 16:52:26 +0900

    금액차트 하는중

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './ChartBar.css';
+
+const ChartBar = (props) => {
+  let barFillHeight = '0%';
+  if (props.max > 0) {
+    // Chart에 데이터가 있다면
+    barFillHeight = Math.round((props.value / props.maxValue) * 100 + '%');
+  }
+
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__-inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }} //css를 동적으로 설정
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
+
+export default ChartBar;
---
+++ src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Chart from '../Chart/Chart';
+
+const ExpensesChart = (props) => {
+  const chartDataPoint = [
+    { label: '1월', value: 0 },
+    { label: '2월', value: 0 },
+    { label: '3월', value: 0 },
+    { label: '4월', value: 0 },
+    { label: '5월', value: 0 },
+    { label: '6월', value: 0 },
+    { label: '7월', value: 0 },
+    { label: '8월', value: 0 },
+    { label: '9월', value: 0 },
+    { label: '10월', value: 0 },
+    { label: '11월', value: 0 },
+    { label: '12월', value: 0 },
+  ];
+
+    for (const expense of props.expenses) // 모든 비용을 살펴서 합산하여 value에 보낸다.
+    { 
+        const expenseMonth = expense.date.getMonth();  // getMonth로 인덱스 0번부터 1월임
+        chartDataPoint[expenseMonth].value += expense.amount; // 월별로 금액을 더하여 value에 대입한다.
+    }
+
+  return <Chart />;
+};
+
+export default ExpensesChart;
